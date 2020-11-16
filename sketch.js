ca9a087d2f2dbe5c598e42537c7c94451ee157da
Chirag Gnanesh <73517004+chirag11022007@users.noreply.github.com>
2020-11-16 11:27:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var ground;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(790, 350, 800, 20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  ground.display();
+}
+
+
+
